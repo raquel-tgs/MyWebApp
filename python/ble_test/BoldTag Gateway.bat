a78b00cc921b803101e7257e1afd8498143a1a39
Bruno <bruno.campetella@timbergrove.com>
2024-11-12 11:56:32 -0600

    fonts changed edit-tag page

---
+++ python/ble_test/BoldTag Gateway.bat
@@ -0,0 +1,2 @@
+CD "C:\Users\Bruno\Documents\GitHub\0002-0002-BLE_PY-Flowlines_Tracking_System\python\ble_test"
+cmd /k C:\Users\Bruno\AppData\Local\Programs\Python\Python312\python.exe poc_server.py
